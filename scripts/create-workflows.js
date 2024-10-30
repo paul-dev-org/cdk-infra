ab13dd807e56fcb6f5ac61b46d8529fbad023b2f
Paweł Rugała <pawelrugala@Pawes-MacBook-Pro.local>
2024-10-30 17:59:32 +0100

    feat: create gh workflows

--- scripts/create-workflows.js
+++ scripts/create-workflows.js
@@ -1,6 +1,8 @@
-import fs from "fs";
+import path from "path";
 
-export const createWorkflows = () => {
+const fs = require("fs");
+
+createWorkflows = () => {
   const workflowsPath = ".github/workflows";
   if (!fs.existsSync(workflowsPath)) {
     fs.mkdirSync(workflowsPath, { recursive: true });
@@ -8,4 +10,17 @@ export const createWorkflows = () => {
   } else {
     console.log(`Workflows directory already exists`);
   }
+
+  //copy from templates/release-please.yaml to .github/workflows/
+  const releasePleaseTemplate = fs.readFileSync(
+    path.resolve(__dirname, "templates/release-please.yaml"),
+    "utf8"
+  );
+
+  fs.writeFileSync(
+    path.resolve(__dirname, workflowsPath, "release-please.yaml"),
+    releasePleaseTemplate
+  );
 };
+
+createWorkflows();
