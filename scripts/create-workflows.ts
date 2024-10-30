0e3cac7c42f7de02aeb5e0401532416bd28a3028
Paweł Rugała <pawelrugala@Pawes-MacBook-Pro.local>
2024-10-30 18:32:32 +0100

    fix: create workflows

---
+++ scripts/create-workflows.ts
@@ -0,0 +1,25 @@
+import * as path from "path";
+import * as fs from "fs";
+
+const createWorkflows = () => {
+  const workflowsPath = ".github/workflows";
+  if (!fs.existsSync(workflowsPath)) {
+    fs.mkdirSync(workflowsPath, { recursive: true });
+    console.log("Workflows directory created");
+  } else {
+    console.log(`Workflows directory already exists`);
+  }
+
+  //copy from templates/release-please.yaml to .github/workflows/
+  const releasePleaseTemplate = fs.readFileSync(
+    path.resolve(__dirname, "templates/release-please.yaml"),
+    "utf8"
+  );
+
+  fs.writeFileSync(
+    path.resolve(__dirname, workflowsPath, "release-please.yaml"),
+    releasePleaseTemplate
+  );
+};
+
+createWorkflows();
